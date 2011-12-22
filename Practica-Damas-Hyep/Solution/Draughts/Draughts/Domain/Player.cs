15e7509fee541451004a82c8c452eb1820a96f14
ilidian_86@hotmail.com <ilidian_86@hotmail.com@7f6749e7-9b37-72dc-0f3f-22f3abd8b4e6>
2011-12-22 11:29:08 +0000

    Clase Player finalizada.

--- Practica-Damas-Hyep/Solution/Draughts/Draughts/Domain/Player.cs
+++ Practica-Damas-Hyep/Solution/Draughts/Draughts/Domain/Player.cs
@@ -7,5 +7,52 @@ namespace Draughts.Domain
 {
     class Player
     {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private string pwd;
+        public string Pwd
+        {
+            get { return pwd; }
+            set { pwd = value; }
+        }
+        private int avatar;
+        public int Avatar
+        {
+            get { return avatar; }
+            set { avatar = value; }
+        }
+        private int ganadas;
+        public int Ganadas
+        {
+            get { return ganadas; }
+            set { ganadas = value; }
+        }
+        private int empatadas;
+        public int Empatadas
+        {
+            get { return empatadas; }
+            set { empatadas = value; }
+        }
+        private int perdidas;
+        public int Perdidas
+        {
+            get { return perdidas; }
+            set { perdidas = value; }
+        }
+
+        public Player(string name, string pwd, int avatar)
+        {
+            this.name = name;
+            this.pwd = pwd;
+            this.avatar = avatar;
+            this.ganadas = 0;
+            this.empatadas = 0;
+            this.perdidas = 0;
+        }
+        
     }
 }
