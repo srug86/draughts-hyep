f2c69d8c17624b964fac8fe066ebf1ef4956e8b9
ilidian_86@hotmail.com <ilidian_86@hotmail.com@7f6749e7-9b37-72dc-0f3f-22f3abd8b4e6>
2011-12-27 17:49:05 +0000

    Agregada clase cola circular y funcionalidad para elegir las fotos, aunque falla una pequeña coseja....

---
+++ Practica-Damas-Hyep/Solution/Draughts/Draughts/Presentation/QueueBounded.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+namespace Draughts.Presentation
+{
+    class BoundedQueue<T>
+    {
+        T[] que;
+        int head;       // remove from head
+        int tail;       // insert at tail
+
+        public BoundedQueue(int quesize)
+        {
+            head = tail = -1;
+            que = new T[quesize];
+        }
+
+        public void enqueue(T elem)  // if next index to tail == head => Q is FULL
+        {
+            int newIndex = nextIndex(tail);
+            if (newIndex == head)
+                Console.Write("Error al introducir valores.");
+            tail = newIndex;
+            que[newIndex] = elem;
+            if (head == -1)
+                head = 0;
+        }
+
+        public T dequeue()  // After removing from head, if that was the only element in Q
+        // Mark Q to be empty by setting head and tail to -1
+        {
+            if (head == -1)
+                Console.Write("Cola vacía.");
+            T elem = que[head];
+            que[head] = default(T);
+            if (head == tail)
+            {
+                head = tail = -1;
+            }
+            else
+            {
+                head = nextIndex(head);
+            }
+            return elem;
+        }
+
+        public int nextIndex(int index)
+        {
+            return (index + 1) % que.Length;
+        }
+        public T getNext(int n)
+        {
+            int i = (n + 1) % que.Length;
+            return que[i];
+        }
+
+        public T getPrev(int m)
+        {
+            int i = (m - 1) % que.Length;
+            return que[i];
+        }
+    }
+}
--- Practica-Damas-Hyep/Solution/Draughts/Draughts/Presentation/SelectWin.xaml.cs
+++ Practica-Damas-Hyep/Solution/Draughts/Draughts/Presentation/SelectWin.xaml.cs
@@ -20,10 +20,23 @@ namespace Draughts.Presentation
     public partial class SelectWin : Window
     {
         InitWin init;
+        BoundedQueue<String> images;
+        int ind;
         public SelectWin(InitWin init)
         {
             this.init = init;
             InitializeComponent();
+            ind = 0;
+            images = new BoundedQueue<String>(9);
+            images.enqueue("Anand(IND).jpg");
+            images.enqueue("Fischer(USA).jpg");
+            images.enqueue("Karpov(URRS).jpg");
+            images.enqueue("Kasimdzhanov(UZB).jpg");
+            images.enqueue("Kasparov(RUS).jpg");
+            images.enqueue("Kramnik(RUS).jpg");
+            images.enqueue("Ponomariov(UCR).jpg");
+            images.enqueue("Spassky(URRS).jpg");
+            images.enqueue("Topalov(BUL).jpg");
         }
 
         private void btnBack_Click(object sender, RoutedEventArgs e)
@@ -44,5 +57,49 @@ namespace Draughts.Presentation
             init.Visibility = Visibility.Visible;
             this.Close();
         }
+        private void btnNext1_Click(object sender, RoutedEventArgs e)
+        {
+            String ruta = images.getNext(ind);
+            BitmapImage bi = new BitmapImage();
+            bi.BeginInit();
+            bi.UriSource = new Uri(ruta, UriKind.Relative);
+            bi.EndInit();
+            Imagej1.Source = bi;
+            ind++;
+        }
+
+        private void btnPrev1_Click(object sender, RoutedEventArgs e)
+        {
+            String ruta = images.getPrev(ind);
+            BitmapImage bi = new BitmapImage();
+            bi.BeginInit();
+            bi.UriSource = new Uri(ruta, UriKind.Relative);
+            bi.EndInit();
+            Imagej1.Source = bi;
+            ind--;
+        }
+
+        private void btnPrev2_Click(object sender, RoutedEventArgs e)
+        {
+            String ruta = images.getPrev(ind);
+            BitmapImage bi = new BitmapImage();
+            bi.BeginInit();
+            bi.UriSource = new Uri(ruta, UriKind.Relative);
+            bi.EndInit();
+            Imagej2.Source = bi;
+            ind--;
+        }
+
+        private void btnNext2_Click(object sender, RoutedEventArgs e)
+        {
+            String ruta = images.getNext(ind);
+            BitmapImage bi = new BitmapImage();
+            bi.BeginInit();
+            bi.UriSource = new Uri(ruta, UriKind.Relative);
+            bi.EndInit();
+            Imagej2.Source = bi;
+            ind++;
+        }
+
     }
 }
