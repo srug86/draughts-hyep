c2631d4a78ae804986d5b41c3a2be1af0138b3b6
srug86@gmail.com <srug86@gmail.com@7f6749e7-9b37-72dc-0f3f-22f3abd8b4e6>
2011-12-28 21:17:05 +0000

    - Implementado Observer. - La clase SelectWin crea un GameAdmin y este se ocupa de crear GameWin para ser observado por este. - Las fichas se mueven correctamente por el tablero y muestran los movimientos posibles bien. - Todavía no se pueden comer piezas.

---
+++ Practica-Damas-Hyep/Solution/Draughts/Draughts/Domain/DelegateOfTheBox.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Draughts.Domain
+{
+    public class DelegateOfTheBox
+    {
+        public delegate void BoxDelegate();
+        public event BoxDelegate switchBox;
+
+        public object changeContentsBox
+        {
+            set
+            {
+                switchBox();
+            }
+        }
+    }
+}
--- Practica-Damas-Hyep/Solution/Draughts/Draughts/Domain/GameAdmin.cs
+++ Practica-Damas-Hyep/Solution/Draughts/Draughts/Domain/GameAdmin.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Draughts.Presentation;
 
 namespace Draughts.Domain
 {
-    class GameAdmin
+    public class GameAdmin : Subject
     {
+        public List<Observer> observers = new List<Observer>();
+        private int row;
+        private int column;
+        private int state;
+
         private Player pl1, pl2;
         public Player Pl1
         {
@@ -21,25 +27,69 @@ namespace Draughts.Domain
         }
 
         private int[,] table;
-        public int[,] Table
+
+        public int getTable(int row, int column)
         {
-            get { return table; }
-            set { table = value; }
+            return this.table[row, column];
+        }
+        public void setTable(int row, int column, int state)
+        {
+            this.table[row, column] = state;
+            this.row = row;
+            this.column = column;
+            this.state = state;
+            tableValuesHaveChanged();
         }
 
-        int turn;
+        private int turn;
         public int Turn
         {
             get { return turn; }
             set { turn = value; }
         }
 
-        public GameAdmin(Player pl1, Player pl2)
+        private Boolean finish;
+        private Boolean selected;
+
+        public GameAdmin(InitWin init, Player pl1, Player pl2)
         {
             this.pl1 = pl1;
             this.pl2 = pl2;
             this.turn = 1;
+            this.finish = false;
+            this.selected = false;
+            GameWin game = new GameWin(init, this, this);
+            game.Show();
             initTable();
+        }
+
+        // implementación de la interfaz
+        public void registerInterest(Observer obs)
+        {
+            // Agrega un objeto Observer a la colección
+            observers.Add(obs);
+        }
+
+        // método que recorre la colección de Observers llamando a los notify() de cada Observer
+        private void switchBox()
+        {
+            for (int i = 0; i < observers.Count; i++)
+            {
+                Observer obs = (Observer)observers[i];
+                obs.notify(this.row, this.column, this.state);
+            }
+        }
+
+        private void tableValuesHaveChanged()
+        {
+            //Ejecutamos el método creado anteriormente mediante el delegado para notificar a 
+            //todos los observadores del cambio sucedido en el comboBox
+            DelegateOfTheBox oSwitchBox = new DelegateOfTheBox();
+            DelegateOfTheBox.BoxDelegate oBoxDelegate = new DelegateOfTheBox.BoxDelegate(switchBox);
+            oSwitchBox.switchBox += oBoxDelegate;
+            oSwitchBox.changeContentsBox = state;
+
+            oSwitchBox.switchBox -= oBoxDelegate;
         }
 
         private void initTable()
@@ -53,67 +103,81 @@ namespace Draughts.Domain
             {2, 0, 2, 0, 2, 0, 2, 0},
             {0, 2, 0, 2, 0, 2, 0, 2},
             {2, 0, 2, 0, 2, 0, 2, 0} };
-            /*            this.table = new int[8, 8] {
-                        {0, 1, 0, 1, 0, 1, 0, 1},
-                        {1, 0, 1, 0, 1, 0, 1, 0},
-                        {0, 1, 0, 0, 0, 1, 0, 1},
-                        {0, 0, 0, 0, 0, 0, 0, 0},
-                        {0, 0, 0, 0, 0, 0, 0, 0},
-                        {0, 0, 0, 0, 0, 0, 2, 0},
-                        {0, 0, 0, 1, 0, 0, 0, 2},
-                        {2, 0, 2, 0, 0, 0, 2, 0} }; */
         }
 
-        public void printTable()
+        public void selectedBox(int row, int column)
         {
-            for (int i = 7; i >= 0; i--)
+            if (!finish)
             {
-                Console.WriteLine("");
-                for (int j = 0; j < 8; j++)
+                if (!selected)
                 {
-                    if (j == 0) Console.Write(i + 1 + " |\t");
-                    Console.Write(this.table[i, j] + "\t");
+                    if (calculateOptions(row, column))
+                    {
+                        selected = true;
+                    }
                 }
-                Console.WriteLine("");
-                if (i > 0) Console.Write("  |");
+                else
+                {
+                    selected = false;
+                    int result = doPlay(row, column);
+                    if (result > -1)   // Jugada válida
+                    {
+                        if (result != 0)    // Partida finalizada
+                        {
+                            //Console.WriteLine("Partida finalizada...");
+                            //if (result > 0) Console.WriteLine("Ha ganado el jugador " + result);
+                            //else Console.WriteLine("Ha habido un empate");
+                            this.finish = true;
+                        }
+                        else
+                        {
+                            //Console.WriteLine("Jugada REALIZADA.\nCambio de turno...");
+                            // Cambio de turno
+                            if (this.Turn == 1) this.Turn = 2;
+                            else this.Turn = 1;
+                        }
+                    }
+                    else
+                    {
+                        resetOptions();
+                    }
+                }
             }
-            Console.WriteLine("------------------------------------------------------------------");
-            Console.WriteLine("  |\t1\t2\t3\t4\t5\t6\t7\t8\n");
         }
 
         public int doPlay(int row, int column)
         {
             int result = -1;
-            if (this.table[row, column] == this.turn * 11111)   // Movimiento de una ficha
+            if (this.getTable(row, column) == this.turn * 11111)   // Movimiento de una ficha
             {
-                if (row != 0 && row != 7) this.table[row, column] = this.turn * 1;
-                else this.table[row, column] = this.turn * 11;  // La ficha ha coronado
+                if (row != 0 && row != 7) this.setTable(row, column, this.turn * 1);
+                else this.setTable(row, column, this.turn * 11);  // La ficha ha coronado
                 result = 0;
             }
-            else if (this.table[row, column] == this.turn * 111111) // Movimiento de la reina
+            else if (this.getTable(row, column) == this.turn * 111111) // Movimiento de la reina
             {
-                this.table[row, column] = this.turn * 11;
+                this.setTable(row, column, this.turn * 11);
                 result = 0;
             }
             if (result == 0)    // Después de un movimiento se eliminan las otras opciones
                 for (int i = 0; i < 8; i++)
                     for (int j = 0; j < 8; j++)
-                        if (this.table[i, j] > this.turn * 100)
-                            this.table[i, j] = 0;
+                        if (this.getTable(i, j) > this.turn * 100)
+                            this.setTable(i, j, 0);
             return result;
         }
 
         public Boolean calculateOptions(int row, int column)
         {
-            if (this.table[row, column] == this.turn)   // Ficha normal
+            if (this.getTable(row, column) == this.turn)   // Ficha normal
             {
-                this.table[row, column] = this.turn * 111;
+                this.setTable(row, column, this.turn * 111);
                 chipOptions(row, column, false);
                 return true;
             }
-            else if (this.table[row, column] == this.turn * 11)   // Reina
+            else if (this.getTable(row, column) == this.turn * 11)   // Reina
             {
-                this.table[row, column] = this.turn * 1111;
+                this.setTable(row, column, this.turn * 1111);
                 queenOptions(row, column);
                 return true;
             }
@@ -126,20 +190,20 @@ namespace Draughts.Domain
             {
                 if (column - 1 >= 0 && row + 1 < 8)
                 {
-                    if (this.table[row + 1, column - 1] == 0 && !eating)
-                        this.table[row + 1, column - 1] = this.turn * 11111;
-                    else if (this.table[row + 1, column - 1] == 2 * 1 ||
-                        this.table[row + 1, column - 1] == 2 * 11)
+                    if (this.getTable(row + 1, column - 1) == 0 && !eating)
+                        this.setTable(row + 1, column - 1, this.turn * 11111);
+                    else if (this.getTable(row + 1, column - 1) == 2 * 1 ||
+                        this.getTable(row + 1, column - 1) == 2 * 11)
                     {
                         if (column - 2 >= 0 && row + 2 < 8)
                         {
-                            if (this.table[row + 2, column - 2] == 0)
+                            if (this.getTable(row + 2, column - 2) == 0)
                             {
-                                this.table[row + 2, column - 2] = this.turn * 11111;
-                                if (this.table[row + 1, column - 1] == 2 * 1)
-                                    this.table[row + 1, column - 1] = 2 * 111;
-                                else if (this.table[row + 1, column - 1] == 2 * 11)
-                                    this.table[row + 1, column - 1] = 2 * 1111;
+                                this.setTable(row + 2, column - 2, this.turn * 11111);
+                                if (this.getTable(row + 1, column - 1) == 2 * 1)
+                                    this.setTable(row + 1, column - 1, 2 * 111);
+                                else if (this.getTable(row + 1, column - 1) == 2 * 11)
+                                    this.setTable(row + 1, column - 1, 2 * 1111);
                                 chipOptions(row + 2, column - 2, true);
                             }
                         }
@@ -147,20 +211,20 @@ namespace Draughts.Domain
                 }
                 if (column + 1 < 8 && row + 1 < 8)
                 {
-                    if (this.table[row + 1, column + 1] == 0 && !eating)
-                        this.table[row + 1, column + 1] = this.turn * 11111;
-                    else if (this.table[row + 1, column + 1] == 2 * 1 ||
-                        this.table[row + 1, column + 1] == 2 * 11)
+                    if (this.getTable(row + 1, column + 1) == 0 && !eating)
+                        this.setTable(row + 1, column + 1, this.turn * 11111);
+                    else if (this.getTable(row + 1, column + 1) == 2 * 1 ||
+                        this.getTable(row + 1, column + 1) == 2 * 11)
                     {
                         if (column + 2 < 8 && row + 2 < 8)
                         {
-                            if (this.table[row + 2, column + 2] == 0)
+                            if (this.getTable(row + 2, column + 2) == 0)
                             {
-                                this.table[row + 2, column + 2] = this.turn * 11111;
-                                if (this.table[row + 1, column + 1] == 2 * 1)
-                                    this.table[row + 1, column + 1] = 2 * 111;
-                                else if (this.table[row + 1, column + 1] == 2 * 11)
-                                    this.table[row + 1, column + 1] = 2 * 1111;
+                                this.setTable(row + 2, column + 2, this.turn * 11111);
+                                if (this.getTable(row + 1, column + 1) == 2 * 1)
+                                    this.setTable(row + 1, column + 1, 2 * 111);
+                                else if (this.getTable(row + 1, column + 1) == 2 * 11)
+                                    this.setTable(row + 1, column + 1, 2 * 1111);
                                 chipOptions(row + 2, column + 2, true);
                             }
                         }
@@ -171,20 +235,20 @@ namespace Draughts.Domain
             {
                 if (column - 1 >= 0 && row - 1 >= 0)
                 {
-                    if (this.table[row - 1, column - 1] == 0 && !eating)
-                        this.table[row - 1, column - 1] = this.turn * 11111;
-                    else if (this.table[row - 1, column - 1] == 1 ||
-                        this.table[row - 1, column - 1] == 11)
+                    if (this.getTable(row - 1, column - 1) == 0 && !eating)
+                        this.setTable(row - 1, column - 1, this.turn * 11111);
+                    else if (this.getTable(row - 1, column - 1) == 1 ||
+                        this.getTable(row - 1, column - 1) == 11)
                     {
                         if (column - 2 >= 0 && row - 2 >= 0)
                         {
-                            if (this.table[row - 2, column - 2] == 0)
+                            if (this.getTable(row - 2, column - 2) == 0)
                             {
-                                this.table[row - 2, column - 2] = this.turn * 11111;
-                                if (this.table[row - 1, column - 1] == 1)
-                                    this.table[row - 1, column - 1] = 111;
-                                else if (this.table[row - 1, column - 1] == 11)
-                                    this.table[row - 1, column - 1] = 1111;
+                                this.setTable(row - 2, column - 2, this.turn * 11111);
+                                if (this.getTable(row - 1, column - 1) == 1)
+                                    this.setTable(row - 1, column - 1, 111);
+                                else if (this.getTable(row - 1, column - 1) == 11)
+                                    this.setTable(row - 1, column - 1, 1111);
                                 chipOptions(row - 2, column - 2, true);
                             }
                         }
@@ -192,20 +256,20 @@ namespace Draughts.Domain
                 }
                 if (column + 1 < 8 && row - 1 >= 0)
                 {
-                    if (this.table[row - 1, column + 1] == 0 && !eating)
-                        this.table[row - 1, column + 1] = this.turn * 11111;
-                    else if (this.table[row - 1, column + 1] == 1 ||
-                        this.table[row - 1, column + 1] == 11)
+                    if (this.getTable(row - 1, column + 1) == 0 && !eating)
+                        this.setTable(row - 1, column + 1, this.turn * 11111);
+                    else if (this.getTable(row - 1, column + 1) == 1 ||
+                        this.getTable(row - 1, column + 1) == 11)
                     {
                         if (column + 2 < 8 && row - 2 >= 0)
                         {
-                            if (this.table[row - 2, column + 2] == 0)
+                            if (this.getTable(row - 2, column + 2) == 0)
                             {
-                                this.table[row - 2, column + 2] = this.turn * 11111;
-                                if (this.table[row - 1, column + 1] == 1)
-                                    this.table[row - 1, column + 1] = 111;
-                                else if (this.table[row - 1, column + 1] == 11)
-                                    this.table[row - 1, column + 1] = 1111;
+                                this.setTable(row - 2, column + 2, this.turn * 11111);
+                                if (this.getTable(row - 1, column + 1) == 1)
+                                    this.setTable(row - 1, column + 1, 111);
+                                else if (this.getTable(row - 1, column + 1) == 11)
+                                    this.setTable(row - 1, column + 1, 1111);
                                 chipOptions(row - 2, column + 2, true);
                             }
                         }
@@ -226,32 +290,32 @@ namespace Draughts.Domain
                         case 0:
                             if (row + i < 8 && column + i < 8)
                             {
-                                if (this.table[row + i, column + i] == 0)
-                                    this.table[row + i, column + i] = this.turn * 111111;
+                                if (this.getTable(row + i, column + i) == 0)
+                                    this.setTable(row + i, column + i, this.turn * 111111);
                             }
                             else stop = true;
                             break;
                         case 1:
                             if (row + i < 8 && column - i >= 0)
                             {
-                                if (this.table[row + i, column - i] == 0)
-                                    this.table[row + i, column - i] = this.turn * 111111;
+                                if (this.getTable(row + i, column - i) == 0)
+                                    this.setTable(row + i, column - i, this.turn * 111111);
                             }
                             else stop = true;
                             break;
                         case 2:
                             if (row - i >= 0 && column + i < 8)
                             {
-                                if (this.table[row - i, column + i] == 0)
-                                    this.table[row - i, column + i] = this.turn * 111111;
+                                if (this.getTable(row - i, column + i) == 0)
+                                    this.setTable(row - i, column + i, this.turn * 111111);
                             }
                             else stop = true;
                             break;
                         case 3:
                             if (row - i >= 0 && column - i >= 0)
                             {
-                                if (this.table[row - i, column - i] == 0)
-                                    this.table[row - i, column - i] = this.turn * 111111;
+                                if (this.getTable(row - i, column - i) == 0)
+                                    this.setTable(row - i, column - i, this.turn * 111111);
                             }
                             else stop = true;
                             break;
@@ -265,11 +329,11 @@ namespace Draughts.Domain
             for (int i = 0; i < 8; i++)
                 for (int j = 0; j < 8; j++)
                 {
-                    if (this.table[i, j] > 10000) this.table[i, j] = 0;
-                    else if (this.table[i, j] == 111) this.table[i, j] = 1;
-                    else if (this.table[i, j] == 1111) this.table[i, j] = 11;
-                    else if (this.table[i, j] == 2 * 111) this.table[i, j] = 2;
-                    else if (this.table[i, j] == 2 * 1111) this.table[i, j] = 22;
+                    if (this.getTable(i, j) > 10000) this.setTable(i, j, 0);
+                    else if (this.getTable(i, j) == 111) this.setTable(i, j, 1);
+                    else if (this.getTable(i, j) == 1111) this.setTable(i, j, 11);
+                    else if (this.getTable(i, j) == 2 * 111) this.setTable(i, j, 2);
+                    else if (this.getTable(i, j) == 2 * 1111) this.setTable(i, j, 22);
                 }
         }
     }
