6652c7bf142867c37872f3d9825ec1027e48a181
ilidian_86@hotmail.com <ilidian_86@hotmail.com@7f6749e7-9b37-72dc-0f3f-22f3abd8b4e6>
2012-01-02 16:56:22 +0000

    Creada la ventana Login, asi como modificada la ventana profile y realiza la conectividad sin funcionalidad...

---
+++ Practica-Damas-Hyep/Solution/Draughts/Draughts/Presentation/LoginWin.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Navigation;
+using Draughts.Presentation;
+
+namespace Draughts
+{
+	public partial class LoginWin
+	{
+        InitWin init;
+		public LoginWin(InitWin init)
+		{
+            this.init = init;
+			InitializeComponent();
+			
+			// Insert code required on object creation below this point.
+		}
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            init.Visibility = Visibility.Visible;
+            this.Close();
+        }
+
+        private void btnAccept_Click(object sender, RoutedEventArgs e)
+        {
+            ConnectWin conect = new ConnectWin(init);
+            conect.Show();
+            this.Visibility = Visibility.Hidden;
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {   
+            init.Visibility = Visibility.Visible;
+            this.Close();
+        }
+	}
+}
