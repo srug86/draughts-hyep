cf888317c6f5eb4fed0f5958e4753d1228994ac5
ilidian_86@hotmail.com <ilidian_86@hotmail.com@7f6749e7-9b37-72dc-0f3f-22f3abd8b4e6>
2012-01-17 12:44:40 +0000

    Terminado el chat entre jugadores...

--- trunk/Practica-Damas-Hyep/Solution/Draughts/Draughts/Communications/NetMode.cs
+++ trunk/Practica-Damas-Hyep/Solution/Draughts/Draughts/Communications/NetMode.cs
@@ -2,10 +2,200 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Draughts.Presentation;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
+using System.IO;
+using System.Windows.Threading;
+using System.Windows.Documents;
 
 namespace Draughts.Communications
 {
+    /// <summary>
+    /// Clase que permite conectar a dos jugadores (uno a uno) y así poder empezar una partida de damas.
+    /// </summary>
     class NetMode
     {
+        private TcpListener tcpListener = null; //Socket a la espera de punto remoto
+        private TcpClient tcp = null; //Socket de comunicación para el cliente
+        private byte[] dataToSend = null; //Datos a enviar
+        private Thread serverThread = null; //Hilo para el servidor
+        private Thread conectThread = null; //Hilo para la conexion al servidor
+        private Thread clientThread = null; //Hilo para el cliente
+        private ConnectWin c;
+        public NetMode(ConnectWin c)
+        {
+            this.c = c;
+        }
+
+        public void ModeServer(String ip_server, int port_server)
+        {
+            if (tcp == null)
+            {
+                IPHostEntry localHost = Dns.GetHostEntry(Dns.GetHostName());
+                IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Parse(ip_server), port_server);
+                // Instanciamos el canal TCP a la escucha.
+                tcpListener = new TcpListener(localEndPoint);
+                tcpListener.Start();
+                conectThread = new Thread(this.WaitListening);
+                conectThread.Start();
+            }
+        }
+
+        public void ModeClient(String ip_server, String ip_client, int port_server)
+        {
+            if (tcp == null)
+            {
+                tcp = new TcpClient(new IPEndPoint(IPAddress.Parse(ip_client), port_server + 1));
+                LingerOption lingerOption = new LingerOption(false, 1);
+                tcp.LingerState = lingerOption;
+                tcp.Connect(IPAddress.Parse(ip_server), port_server);
+                //Crear hilo para que esté atento a lo que le envían
+                clientThread = new Thread(this.InfiniteListening);
+                clientThread.Start();
+            }
+        }
+
+        private void WaitListening()
+        {
+            for (; ; )
+            {
+                Thread.Sleep(100);
+                // Si existe alguna petición se acepta y se crea un objeto con la conexión
+                if (tcpListener.Pending())
+                {
+                    try
+                    {
+                        tcp = tcpListener.AcceptTcpClient();
+                        serverThread = new Thread(this.InfiniteListening);
+                        serverThread.Start();
+                    }
+                    catch (Exception e)
+                    {
+                        e.ToString();
+                    }
+                }
+            }
+
+        }
+
+        private void InfiniteListening()
+        {
+            for (; ; )
+            {
+                Thread.Sleep(100);
+                byte[] msg = new Byte[Constants.maxNoOfBytes];
+                byte count1 = 0x01;
+                for (int i = 0; i < msg.Length; i++)
+                {
+                    msg[i] = count1++;
+                }
+                try
+                {
+                    //Leemos el buffer por si se ha recibido algo
+                    int readBytes = tcp.GetStream().Read(msg, 0, msg.Length);
+                    if (checkOff(readBytes, msg))
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        //intentamos recoger el mensaje y ponerlo en pantalla
+                        StringBuilder str = new StringBuilder(Constants.maxNoOfBytes);
+                        for (int count = 0; count < readBytes; count++)
+                        {
+                            char ch = (char)msg[count];
+                            str = str.Append(ch);
+                        }
+                        //Pintamos el mensaje a través de un delegado.
+                        c.delegateToRcvMsgs(str.ToString());
+                    }
+                }
+                catch (IOException)
+                {
+                    return;
+                }
+            }
+        }
+
+        public void sendMsg(String msg)
+        {
+            if (tcp != null)
+            {
+                if (tcp.Connected)
+                {
+                    if (msg.Length != 0)
+                    {
+                        //Escribir mensaje en la ventana local
+                        msg = ">> " + msg;
+                        //Prepara mensaje para enviar como array de bytes
+                        char[] charArray = msg.ToCharArray(0, msg.Length);
+                        dataToSend = new byte[msg.Length];
+                        for (int charCount = 0; charCount < msg.Length; charCount++)
+                        {
+                            dataToSend[charCount] = (byte)charArray[charCount];
+                        }
+                        //Usamos el socket para enviar el mensaje
+                        tcp.GetStream().Write(dataToSend, 0, dataToSend.Length);
+                    }
+                }
+            }
+        }
+        private void FreeResources(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (tcp != null)
+            {
+                if (tcp.Connected)
+                {
+                    this.sendOff();
+                    if (tcp != null) tcp.GetStream().Close();
+                    if (tcpListener != null) tcpListener.Stop();
+                    if (tcp != null) tcp.Close();
+                    tcp = null;
+                }
+            }
+        }
+
+        private void sendOff()
+        {
+            dataToSend = new byte[] { (byte)'s', (byte)'h', (byte)'u', (byte)'t', (byte)'d', (byte)'o', (byte)'w', (byte)'n' };
+            tcp.GetStream().Write(dataToSend, 0, dataToSend.Length);
+        }
+
+        private bool checkOff(int size, byte[] msg)
+        {
+            //Si me envian "shutdown", desconecto los sockets
+            bool exit = false;
+            if (size == 8)
+            {
+                StringBuilder shutMessage = new StringBuilder(8);
+                for (int count = 0; count < 8; count++)
+                {
+                    char ch = (char)msg[count];
+                    shutMessage = shutMessage.Append(ch);
+                }
+                string shut = "shutdown";
+                string receivedMessage = shutMessage.ToString();
+                if (receivedMessage.Equals(shut))
+                {
+                    c.delegateToRcvMsgs("--Desconectado--");
+                    if (tcp != null)
+                        tcp.GetStream().Close();
+                    if (tcpListener != null)
+                        tcpListener.Stop();
+                    if (tcp != null)
+                        tcp.Close();
+                    exit = true;
+                }
+            }
+            return exit;
+        }
+    }
+
+    public class Constants
+    {
+        public const int noOfBytesToReadSuperPacket = 4;
+        public const int maxNoOfBytes = 784;
     }
 }
